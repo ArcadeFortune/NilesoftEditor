c4ccb86964d44d98fc21a707aa85d0249c10ded7
ArcadeFortune <alessionights@gmail.com>
2024-08-04 14:52:17 +0200

    convert file to json

--- NilesoftEditor.js
+++ NilesoftEditor.js
@@ -33,11 +33,13 @@ const INPUT_OPTIONS = {sep: 'both'};
  * @param {Object} options additional options like 'sep' or 'vis'
  * @returns {boolean} true if the input is valid, or a string with the error message
  */
-function validateInput(position, section, title, imagePath, cmd, options) {
-  if (position < 0) return ('Invalid position: ' + position);
-  if (!SECTIONS.includes(section)) return ('Invalid section: ' + section);
-  if (!title) title = 'noTitle';
-  if (!cmd) return ('No command specified: ' + cmd);
+function validateInput(input) {
+  if (!input) return
+  if (!input.section) return ('invalid input')
+  if (input.position < 0) return ('Invalid position: ' + input.position);
+  if (!SECTIONS.includes(input.section)) return ('Invalid section: ' + input.section);
+  if (!input.title) input.title = 'noTitle';
+  if (!input.cmd) return ('No command specified: ' + input.cmd);
   //todo options validation
   return true;
 }
@@ -76,7 +78,7 @@ function calculateIndents(position) {
  * @param {Object} options additional options like 'sep' or 'vis'
  * @returns {String} the text string to be inserted in the file
  */
-function createTextString(indents=0, section, title='noTitle', imagePath, cmd, options) {
+function createTextString(indents=0, input) {
   let result = '';
   const tabs = '\t'.repeat(indents)
   const newLine = '\n'
@@ -85,19 +87,19 @@ function createTextString(indents=0, section, title='noTitle', imagePath, cmd, o
   result += tabs
 
   //create the text string
-  result = `${section}(title='${title}' image='${imagePath}' `;
+  result = `${input.section}(title='${input.title}' image='${input.imagePath}' `;
 
   //command
-  if (section === 'item') result += `cmd='${cmd}' `;
+  if (input.section === 'item') result += `cmd='${input.cmd}' `;
 
   //additional options
-  if (options.sep) result += `sep=${options.sep} `;
-  if (options.vis) result += `vis=${options.vis} `;
+  if (input.sep) result += `sep=${input.sep} `;
+  if (input.vis) result += `vis=${input.vis} `;
 
   result += ')';
 
   //menu
-  if (section === 'menu') result += newLine + tabs + '{' + newLine + tabs + '}';
+  if (input.section === 'menu') result += newLine + tabs + '{' + newLine + tabs + '}';
 
   return result
 }
@@ -142,12 +144,155 @@ function applyChanges() {
   exec(`powershell -Command "Start-Process cmd -ArgumentList '/c ${command}' -Verb RunAs"`);
 }
 
+/**
+ * Function to convert a row string from a .nss file to a json object.
+ * @param {String} rowString row as a string from a .nss file
+ * @returns {Object} a json object with the row content
+ */
+function convertRowToJson(rowString='') {
+  let temp = rowString.trim().split('item(');
+  let content;
+  let type;
+  let result = {};
+  if (temp.length > 1) {
+    //its an item
+    type = 'item'
+  }
+  else {
+    //its a menu
+    temp = temp.join('').split('menu(');
+    type = 'menu';
+  }
 
-// main logic
-const ok = validateInput(INPUT_POSITION, INPUT_SECTION, INPUT_TITLE, INPUT_IMAGE_PATH, INPUT_CMD, INPUT_OPTIONS);
-if (ok !== true) return console.error(ok);
-const indents = calculateIndents(INPUT_POSITION);
-const text = createTextString(indents, INPUT_SECTION, INPUT_TITLE, INPUT_IMAGE_PATH, INPUT_CMD, INPUT_OPTIONS);
-addRow(path, text, INPUT_POSITION, indents);
-applyChanges();
+
+  result[type] = [];
+  content = temp[1];
+    let fragmented;
+    fragmented = content.split('=');
+
+    for (let i = 1; i < fragmented.length; i++) {
+      const currentString = fragmented[i];
+      const prevString = fragmented[i - 1];
+      const lastSpaceIndex = currentString.lastIndexOf(' ')
+      const prevLastSpaceIndex = prevString.lastIndexOf(' ');
+
+      // Split the string into two parts: before the last space and after the last space
+      const firstPart = currentString.substring(0, lastSpaceIndex) || currentString.slice(0, -1);;
+      const secondPart = currentString.substring(lastSpaceIndex + 1);
+      const prevFirstPart = prevString.substring(0, prevLastSpaceIndex);
+      const prevSecondPart = prevString.substring(prevLastSpaceIndex + 1);
+
+      //save a result
+      const obj = {};
+      obj[prevSecondPart] = firstPart;
+      result[type].push(obj);
+    }
+  return result;
+}
+/**
+ * function to process the lines of a .nss file and convert them to json objects
+ * @param {Array} lines array of strings representing the lines of a .nss file
+ * @returns {Array} an array of json objects representing the content of the file
+ */
+function processLines(lines=[]) {
+  const result = [];
+  let i = 0;
+
+  function parseBlock() {
+      const block = [];
+      while (i < lines.length) {
+          let line = lines[i].trim();
+          if (line === '{') {
+              i++;
+              block[block.length - 1].menu.push({children: parseBlock()})
+              // block[block.length - 1].children = parseBlock(); //less nested
+          } else if (line === '}') {
+              i++;
+              return block;
+          } else {
+              block.push(convertRowToJson(line));
+              i++;
+          }
+      }
+      return block;
+  }
+
+  result.push(...parseBlock());
+  return result;
+}
+
+//start http server
+const http = require('http');
+const querystring = require('querystring');
+const port = 65535;
+const parseBody = async (req) => {
+  //fills req.body with the parsed json
+  return new Promise((resolve, reject) => {
+    req.body = '';
+    req.on('data', chunk => {
+      req.body += chunk.toString();
+    })
+    req.on('end', () => {
+      // if (!req.body) resolve();
+      // else {
+      if (req.headers['content-type'].includes('application/x-www-form-urlencoded')) {
+        req.body = querystring.parse(req.body);
+      } else if (req.headers['content-type'].includes('application/json')) {
+        req.body = JSON.parse(req.body);
+      }
+      // }
+      resolve();
+    })
+  })
+}
+const server = http.createServer(async (req, res) => {
+  if (req.url === '/favicon.ico') return;
+  console.log('got new connection', req.url);
+
+  if (req.url === '/test') {
+    if (req.method === 'POST') {
+      await parseBody(req);
+      const input = req.body;
+      
+      // main logic
+      const ok = validateInput(input);
+      if (ok === true) {
+        const indents = calculateIndents(input.position);
+        const text = createTextString(indents, input);
+        addRow(path, text, input.position, indents);
+      }
+      else {
+        console.error('error:', ok);
+      }
+      res.end('<h1>hehehehaw</h1>')
+    }
+    return;
+  }
+
+  if (req.url === '/apply') {
+    applyChanges();
+    res.end()
+    return;
+  }
+
+  //every other url
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(fs.readFileSync('index.html'));
+});
+
+// server.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}/`);
+// });
+
+
+// Read the file content
+const data = fs.readFileSync(path, 'utf8');
+
+// Split the file content into lines
+const lines = data.split('\n');
+
+const result = processLines(lines);
+
+console.log(JSON.stringify(result, null, 2));
 
